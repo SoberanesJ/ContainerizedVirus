c8019625ea7f9ad60c9e3ba30046ffca07b0a3c6
Juan Soberanes <45519261+johnfsu@users.noreply.github.com>
2020-03-26 15:13:53 -0700

    Update Installwindowsxp.sh

--- Installwindowsxp.sh
+++ Installwindowsxp.sh
@@ -1,3 +1,10 @@
+#Update and Upgrade the Ubuntu System and Repositories
+sudo su
+apt update && upgrade
+apt install vim -y
+apt install virtualbox -y 
+
+
 #This file is installed inside of the Ubuntu 16.04 container
 mkdir -p /vbox/vdisks
 mkdir -p /vbox/iso
@@ -10,14 +17,14 @@ VBoxManage createhd --filename /vbox/vdisks/winxp32.vdi --size 10240 --variant S
 VBoxManage storagectl winxp32 --name "IDE Controller" --add ide --controller PIIX4 --bootable on
 
 #Replace the iso file with your preferred iso
-VBoxManage storageattach winxp32 --storagectl "IDE Controller" --type dvddrive --port 1 --device 0 --medium /vbox/iso/w2k3sp2_3959_usa_x64fre_spcd.iso
+VBoxManage storageattach winxp32 --storagectl "IDE Controller" --type dvddrive --port 1 --device 0 --medium /vbox/iso/
 
 
 
 VBoxManage storageattach winxp32 --storagectl "IDE Controller" --type hdd --port 0 --device 0 --medium /vbox/vdisks/winxp32.vdi
 
 
-VBoxManage modifyvm winxp32 --dvd /vbox/iso/w2k3sp2_3959_usa_x64fre_spcd.iso
+VBoxManage modifyvm winxp32 --dvd /vbox/iso/
 
 
 VBoxManage modifyvm winxp32 --boot1 dvd
@@ -36,5 +43,9 @@ VBoxManage modifyvm winxp32 --natpf1 "smb139,tcp,,139,10.0.2.15,139"
 
 VBoxManage modifyvm winxp32 --natpf1 "smb445,tcp,,445,10.0.2.15,445"
 
-
 VBoxManage modifyvm winxp32 --natpf1 "lport4444,tcp,,4444,10.0.2.15,4444"
+#Set Password
+VBoxManage modifyvm winxp32 --vrdeproperty VNCPassword=secret
+
+#Start Windows XP
+VBoxHeadless -v on -startvm winxp32
