59e5d702df388d4277ef3459b2e4641060193005
Juan Soberanes <45519261+johnfsu@users.noreply.github.com>
2020-03-26 20:43:32 -0700

    Update Full _Install.sh

--- Full _Install.sh
+++ Full _Install.sh
@@ -94,37 +94,33 @@ sed -i 's/3389/3391/1' /etc/xrdp/xrdp.ini
 #Load up Docker and the container
 apt-get install linux-headers-gcp
 
+#Download iso from bucket
+gsutil cp gs://nestedvm/Xphomesp3.iso /home/baccc2/Downloads/
+
+
+cd /home/baccc2/Downloads/
 docker pull ubuntu:latest
 docker ps
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#Copy over iso to container
+CID=$(sudo docker ps > dockerid.txt &&  tail -1 dockerid.txt  |awk '{print $1;}')
+docker cp Xphomesp3.iso $CID:/
 
 
 #####This is all done inside of the container####
 #Update and Upgrade the Ubuntu System and Repositories
 sudo su
 apt update && upgrade
+apt install sudo
 apt install vim -y
 apt install virtualbox -y 
 
 #This file is installed inside of the Ubuntu 16.04 container
 mkdir -p /vbox/vdisks
 mkdir -p /vbox/iso
+cd /
+mv Xphomesp3.iso vbox/iso/
+
 VBoxManage createvm --name winxp32 --register --basefolder /vbox/vms/
 
 
@@ -134,14 +130,14 @@ VBoxManage createhd --filename /vbox/vdisks/winxp32.vdi --size 10240 --variant S
 VBoxManage storagectl winxp32 --name "IDE Controller" --add ide --controller PIIX4 --bootable on
 
 #Replace the iso file with your preferred iso
-VBoxManage storageattach winxp32 --storagectl "IDE Controller" --type dvddrive --port 1 --device 0 --medium /vbox/iso/
+VBoxManage storageattach winxp32 --storagectl "IDE Controller" --type dvddrive --port 1 --device 0 --medium /vbox/iso/Xphomesp3.iso
 
 
 
 VBoxManage storageattach winxp32 --storagectl "IDE Controller" --type hdd --port 0 --device 0 --medium /vbox/vdisks/winxp32.vdi
 
 
-VBoxManage modifyvm winxp32 --dvd /vbox/iso/
+VBoxManage modifyvm winxp32 --dvd /vbox/iso/Xphomesp3.iso
 
 
 VBoxManage modifyvm winxp32 --boot1 dvd
