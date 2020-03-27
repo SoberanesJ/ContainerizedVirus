51d109fb093a079b2ed826cbe96a04383a456627
Juan Soberanes <45519261+johnfsu@users.noreply.github.com>
2020-03-26 22:07:34 -0700

    Update Installwindowsxp.sh

--- Installwindowsxp.sh
+++ Installwindowsxp.sh
@@ -1,13 +1,17 @@
+#####This is all done inside of the container####
 #Update and Upgrade the Ubuntu System and Repositories
 sudo su
 apt update && upgrade
+apt install sudo
 apt install vim -y
 apt install virtualbox -y 
-
 
 #This file is installed inside of the Ubuntu 16.04 container
 mkdir -p /vbox/vdisks
 mkdir -p /vbox/iso
+cd /
+mv Xphomesp3.iso vbox/iso/
+
 VBoxManage createvm --name winxp32 --register --basefolder /vbox/vms/
 
 
@@ -17,14 +21,14 @@ VBoxManage createhd --filename /vbox/vdisks/winxp32.vdi --size 10240 --variant S
 VBoxManage storagectl winxp32 --name "IDE Controller" --add ide --controller PIIX4 --bootable on
 
 #Replace the iso file with your preferred iso
-VBoxManage storageattach winxp32 --storagectl "IDE Controller" --type dvddrive --port 1 --device 0 --medium /vbox/iso/
+VBoxManage storageattach winxp32 --storagectl "IDE Controller" --type dvddrive --port 1 --device 0 --medium /vbox/iso/Xphomesp3.iso
 
 
 
 VBoxManage storageattach winxp32 --storagectl "IDE Controller" --type hdd --port 0 --device 0 --medium /vbox/vdisks/winxp32.vdi
 
 
-VBoxManage modifyvm winxp32 --dvd /vbox/iso/
+VBoxManage modifyvm winxp32 --dvd /vbox/iso/Xphomesp3.iso
 
 
 VBoxManage modifyvm winxp32 --boot1 dvd
