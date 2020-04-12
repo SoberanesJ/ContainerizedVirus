b1ca44fe7067bd96cec3ce806228d330a3ea9f6d
Juan Soberanes <45519261+johnfsu@users.noreply.github.com>
2020-04-11 19:06:13 -0700

    Update Installwindowsxp.sh
    Optimized for performance

--- Installwindowsxp.sh
+++ Installwindowsxp.sh
@@ -1,35 +1,18 @@
 #####This is all done inside of the container####
 #Update and Upgrade the Ubuntu System and Repositories
 apt update 
-
 apt install virtualbox -y 
 apt install virtualbox-dkms -y
- 
 apt install linux-headers-gcp -y
 apt install linux-headers-generic -y 
 
-#This file is installed inside of the Ubuntu 16.04 container
-mkdir -p /vbox/vdisks
-mkdir -p /vbox/iso
 cd /home/baccc2/container/
-mv Xphomesp3.iso /vbox/iso/
 
-VBoxManage createvm --name winxp32 --register --basefolder /vbox/vms/
+#Import VM 
+VBoxManage import winxp_1.ova
 
-VBoxManage createhd --filename /vbox/vdisks/winxp32.vdi --size 10240 --variant Standard
-
-VBoxManage storagectl winxp32 --name "IDE Controller" --add ide --controller PIIX4 --bootable on
-#Replace the iso file with your preferred iso
-VBoxManage storageattach winxp32 --storagectl "IDE Controller" --type dvddrive --port 1 --device 0 --medium /vbox/iso/Xphomesp3.iso
-VBoxManage storageattach winxp32 --storagectl "IDE Controller" --type hdd --port 0 --device 0 --medium /vbox/vdisks/winxp32.vdi
-VBoxManage modifyvm winxp32 --dvd /vbox/iso/Xphomesp3.iso
-VBoxManage modifyvm winxp32 --boot1 dvd
-VBoxManage modifyvm winxp32 --boot2 disk
-VBoxManage modifyvm winxp32 --memory 1024
-VBoxManage modifyvm winxp32 --nic1 nat
-VBoxManage modifyvm winxp32 --cableconnected1 off
 #Set Password
-VBoxManage modifyvm winxp32 --vrdeproperty VNCPassword=secret
+VBoxManage modifyvm winxp_1 --vrdeproperty VNCPassword=secret
 
 #Start Windows XP
-VBoxHeadless -v on -startvm winxp32
+VBoxHeadless -v on -startvm winxp_1
